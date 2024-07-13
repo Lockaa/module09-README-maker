bb248d2207105b40350bb0fe303b81395e7e313a
Lockaa <143623393+Lockaa@users.noreply.github.com>
2024-07-12 20:07:45 -0400

    have-the-prompts

--- Develop/index.js
+++ Develop/index.js
@@ -1,51 +1,47 @@
 // TODO: Include packages needed for this application
-inquirer = require('inquirer');
-var fs = require('fs');
+const inquirer = require('inquirer');
+const fs = require('fs');
 
 // TODO: Create an array of questions for user input
 function getQuestions() {
-    const questions = [];
-    inquirer.prompt([
+    inquirer
+    .prompt([
         {
-        type: 'input',
-        name: 'firstName',
-        message: 'Please type your first name'
+            type: "input",
+            message: "What is your name?",
+            name: "name"
         },
         {
-        type: 'input',
-        name: 'lastName',
-        message: 'Please type your last name'
-        },
-        {
-        type: 'number',
-        name: 'age',
-        message: 'Please type your age'
-        },
-        {
-        type: 'list',
-        name: 'language',
-        message: 'Please select your favorite programming language',
-        choices: ['JS', 'Python', 'Java', 'Ruby', 'C++']
+             type: "input",
+             message: "what is your location?",
+             name: "location"
+         },
+         {
+             type: "input",
+             message: "Please put your linkedIn url here:",
+             name: "linked"
+         },
+         {
+             type: "input",
+             message: "Please put your github url here:",
+             name: "github"
         }
-    ]).then((answerObj) => {
-        console.log(answerObj);
-    });
-
-    questions = answerObj;
-    return questions;
+    ])
+    .then((data) => {
+        writeToFile('./index.html', data);
+    })
 }
-  // TODO: Create a function to write README file
+
+// TODO: Create a function to write README file
 function writeToFile(fileName, data) {
     fs.writeFile(fileName, JSON.stringify(data),function (err){
-        console.log(err)
     })
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    var fileName = ".. /README.md"
-    var answers = getQuestions();
-    writeToFile(fileName, answers);
+    //var fileName = ".. /README.md"
+    getQuestions();
 }
 
 // Function call to initialize app
