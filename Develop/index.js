d6594bde4b2090f2d2b39ae592c71978717e4c47
Lockaa <143623393+Lockaa@users.noreply.github.com>
2024-07-14 21:13:27 -0400

    finished

--- Develop/index.js
+++ Develop/index.js
@@ -1,57 +1,79 @@
-// TODO: Include packages needed for this application
+//Packages
+
 const inquirer = require('inquirer');
 const fs = require('fs');
+const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
-function getQuestions() {
-    inquirer
-    .prompt([
-        {
-            type: "input",
-            message: "Enter Project Title: ",
-            name: "title"
-        },
-        {
-             type: "input",
-             message: "Enter a description: ",
-             name: "description"
-         },
-         {
-             type: "input",
-             message: "Enter Installation details: ",
-             name: "installation"
-         },
-         {
-             type: "input",
-             message: "Credits:",
-             name: "credit"
-         },
-         {
-            type: "list",
-            message: "choose a license: ",
-            choices: ["MIT", "Standard", "Git"],
-            name: "license"
-         }
-    ])
-    .then((data) => {     
-        writeToFile('./sampleREADME.md', data);
-    })
-}
+
+const questions = [
+    {
+        type: "input",
+        message: "Enter the title of your project: ",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter the description of your project: ",
+        name: "description"
+    },
+    {
+        type: "input",
+        message: "Enter the installation directions for your project: ",
+        name: "install"
+    },
+    {
+        type: "input",
+        message: "Enter the usage information for your project: ",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Enter the contribution guidelines for your project: ",
+        name: "contrib"
+    },
+    {
+        type: "input",
+        message: "Enter the test intructions for your project: ",
+        name: "test"
+    },
+    {
+        type: "list",
+        message: "Ehoose which type of licence your program has: ",
+        name: "license",
+        choices: ["MIT", "Apache 2.0", "None"]
+    },
+    {
+        type: "input",
+        message: "Enter your github username:",
+        name: "github"
+    },
+    {
+        type: "input",
+        message: "Enter your email address: ",
+        name: "email"
+    }
+];
+
 
 // TODO: Create a function to write README file
-function writeToFile(fileName, data) {
-    var outStr =    '# ' + JSON.stringify(data.title) + '\n' +
-                    '## discription: \n' + JSON.stringify(data.description) + '\n\n' +
-                    '## installion: \n' + JSON.stringify(data.installation) + '\n\n' +
-                    "## credits: \n" + JSON.stringify(data.credit) + '\n\n' +
-                    '## license: \n' + JSON.stringify(data.license) + '\n\n'
-    fs.writeFile(fileName, outStr,function (err){
+function writeToFile(data) {
+    fs.writeFile('output/README.md', data , (error) => {
+        if(error){
+            return console.log(error);
+        }else{
+            console.log('File made successfully.');
+        }
     })
 }
 
 // TODO: Create a function to initialize app
 function init() {
-    getQuestions();
+    inquirer
+    .prompt(questions)
+    .then((data) => {
+        writeToFile(generateMarkdown(data));
+    })
 }
 
 // Function call to initialize app
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,80 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if(license === "MIT"){
+    return '![Static Badge](https://img.shields.io/badge/License-MIT-blue)';
+  }else if(license === "Apache 2.0"){
+    return '![Static Badge](https://img.shields.io/badge/License-Apache_2.0-purple)';
+  }else{
+    return '';
+  }
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  switch (license) {
+    case 'MIT':
+        return 'https://opensource.org/licenses/MIT';
+    case 'Apache 2.0':
+        return 'https://www.apache.org/licenses/LICENSE-2.0';
+    default:
+        return '';
+}
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+    const link = renderLicenseLink(license);
+    return `Click the following link to get more information on the Licensing : ${link}`
+
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
-  return `# ${data.title}
+  const licenseBadge = renderLicenseBadge(data.license);
+  const licenseSection = renderLicenseSection(data.license);
+  return `
+  # ${data.title} ${licenseBadge}
+
+  ## Description
+
+  ${data.description}
+
+  ## Table of Contents 
+
+  - [Installation](#installation)
+  - [Usage](#usage)
+  - [License](#license)
+  - [Questions](#questions)
+
+  ## Installation
+
+  ${data.install}
+
+  ## Usage
+
+  ${data.usage}
+
+  ## License
+
+  ${licenseSection}
+
+  ## How to Contribute
+
+  ${data.contrib}
+
+  ## Tests
+
+  ${data.test}
+
+  ## Questions
+
+  If you have any questions my github username is : ${data.github}
+  The link to my github is : https://github.com/${data.github}
+  And you can email me at: ${data.email}
+
 
 `;
 }
